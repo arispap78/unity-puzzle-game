af909fd26e4bab843680aedc4c8897cabc4b3d45
arispap78 <73643527+arispap78@users.noreply.github.com>
2021-04-05 01:31:20 +0300

    make children invisible

--- Assets/Scripts/PlacementArea/PlacementArea.cs
+++ Assets/Scripts/PlacementArea/PlacementArea.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class PlacementArea : MonoBehaviour
 {
     public List<GameObject> myPieces = new List<GameObject>();
+    Color tempColor;
     // Start is called before the first frame update
     void Start()
     {
         foreach (Transform tr in transform) 
         {
             myPieces.Add(tr.gameObject);
+            tempColor=tr.gameObject.GetComponent<Renderer>().material.color;
+            tempColor.a = 0f;
+            tr.gameObject.GetComponent<Renderer>().material.color = tempColor;
+
         }
     }
 
--- Assets/Scripts/SceneManager/GameController/GameController.cs
+++ Assets/Scripts/SceneManager/GameController/GameController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public PlacementArea[] activeAreas;// = new List<PlacementArea>();
+    string[] children = new string[] { "red", "blue", "yellow", "green", "brown" };
+    public int choice;
     // Start is called before the first frame update
     private void Awake()
     {
@@ -14,6 +16,8 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        var rand = new System.Random();
+        choice = rand.Next(children.Length);
+        System.Console.Write($"name: {children[choice]}");
     }
 }
